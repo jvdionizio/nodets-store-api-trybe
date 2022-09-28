1d07d798626f465720c69e10ad7e4c310fcf231c
jvdionizio <dionizio145@gmail.com>
2022-09-27 21:19:23 -0300

    feat: post "/users"

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productsRoutes from './routes/productsRoutes';
+import usersRoutes from './routes/usersRoutes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productsRoutes);
+app.use('/users', usersRoutes);
 
 export default app;
